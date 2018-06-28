19860386bc61866f720c3c29bab4d5ca11bb7bb4
king3366ster <king3366ster@gmail.com>
2018-06-28 15:57:54 +0800

    小程序header

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,9 +4,18 @@ const app = getApp()
 
 Page({
   data: {
-    avatar: 'https://yx-web.nos-hz.163yun.com/webdoc/h5/im/default-icon.png'
+    avatar: 'https://yx-web.nos-hz.163yun.com/webdoc/h5/im/default-icon.png',
+    nick: '匿名',
   },
   onLoad() {
-    
+    const userInfo = app.globalData.userInfo
+    if (userInfo) {
+      const {nickName, avatarUrl} = userInfo
+      console.log(userInfo)
+      this.setData({
+        avatar: avatarUrl,
+        nick: nickName
+      })
+    }
   },
 })
